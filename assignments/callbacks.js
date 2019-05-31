802d56de499e8be624010317d74c40b709f51e4e
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-05-30 20:30:26 -0700

    completed Call backs

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -42,16 +42,32 @@ last(items, function(last) {
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(3, 5, function(add) {
+  console.log(add);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(3, 5, function(multiply) {
+  console.log(multiply);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return true;
+    }
+  }
 }
+contains("gum", items, function(abc) {
+  console.log(abc);
+});
 
 /* STRETCH PROBLEM */
 
